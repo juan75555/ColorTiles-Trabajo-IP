dcf6e5d19c8a0737b67b2d679c69a4a3625b02b3
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 16:26:21 +0100

    Update TestTablero.java

--- Consola/TestTablero.java
+++ Consola/TestTablero.java
@@ -1,10 +1,20 @@
-
+import java.util.Scanner;
 public class TestTablero {
-
+	public static void Juego(Tablero t,int[][] tablero) {
+		Scanner teclado = new Scanner(System.in);
+		while(t.getVidas()>0) {
+			System.out.print("\nIntroduce tu jugada (fila, columna): ");
+			int jf = teclado.nextInt();
+			int jc = teclado.nextInt();
+			t.Jugada(jf, jc);
+			t.ImprimeTablero(tablero);
+		}
+	}
 	public static void main(String[] args) {
 		Tablero t = new Tablero();
 		int[][] tablero = t.GeneraTablero();
 		t.ImprimeTablero(tablero);
+		Juego(t,tablero);
 	}
 
 }
