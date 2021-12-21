8d6e2e787434b89afb5b9b21d32e89b2ec6f46eb
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 18:44:43 +0100

    Create TableroDefinitivo.java

---
+++ Consola/TableroDefinitivo.java
@@ -0,0 +1,281 @@
+
+public class Tablero {
+	private int filas,columnas,vidas,puntos;
+	private double dificultad;
+	private int[][] matriz = new int[filas][columnas];
+	public static final String ANSI_BLACK = "\033[30m";
+	public static final String ANSI_RED = "\033[31m";
+	public static final String ANSI_GREEN = "\033[32m";
+	public static final String ANSI_YELLOW = "\033[33m";
+	public static final String ANSI_BLUE = "\033[34m";
+	public static final String ANSI_PURPLE = "\033[35m";
+	public static final String ANSI_CYAN = "\033[36m";
+	public static final String ANSI_WHITE = "\033[37m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
+    public static final String RED_BACKGROUND = "\033[41m";    // RED
+    public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
+    public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+    public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
+    public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+    public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
+    public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
+	Tablero() {
+		//El constructor debe tener los parámetros oportunos 
+		//para inicializar el tablero y el juego
+		this.setFilas(6);
+		this.setColumnas(6);
+		this.setDificultad(0.6);
+		this.setVidas(3);
+		this.setPuntos(0);
+		matriz = new int[getFilas()][getColumnas()];
+		this.setMatriz(GeneraTablero());
+	}
+	
+	public int[][] getMatriz() {
+		return matriz;
+	}
+
+	public void setMatriz(int[][] matriz) {
+		this.matriz=matriz;
+	}
+
+	public double getDificultad() {
+		return dificultad;
+	}
+
+	public void setDificultad(double dificultad) {
+		this.dificultad = dificultad;
+	}
+
+	public int getFilas() {
+		return filas;
+	}
+	public void setFilas(int filas) {
+		this.filas = filas;
+	}
+	public int getColumnas() {
+		return columnas;
+	}
+	public void setColumnas(int columnas) {
+		this.columnas = columnas;
+	}
+	public int getVidas() {
+		return vidas;
+	}
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	public int[][] GeneraTablero() {
+		for(int i = 0; i<getFilas(); i++) {
+			for(int j = 0; j<getColumnas();j++) {
+				matriz[i][j] = (int) (Math.random()*7);
+			}
+		}
+		return matriz;
+	}
+	public void ColoresTablero(int numero) {
+		if(numero==0) {
+			System.out.print(WHITE_BACKGROUND+"   ");
+		}
+		else if(numero==1) {
+			System.out.print(RED_BACKGROUND+"   ");
+		}
+		else if (numero==2) {
+			System.out.print(BLUE_BACKGROUND+"   ");
+		}
+		else if (numero==3) {
+			System.out.print(GREEN_BACKGROUND+"   ");
+		}
+		else if(numero==4) {
+			System.out.print(PURPLE_BACKGROUND+"   ");
+		}
+		else if (numero==5) {
+			System.out.print(YELLOW_BACKGROUND+"   ");
+		}
+		else {
+			System.out.print(CYAN_BACKGROUND+"   ");
+		}
+		System.out.print(ANSI_RESET);
+	}
+	public void ImprimeTablero(int[][] matriz) {
+		for(int i = 0; i<getColumnas();i++) {
+			System.out.print("---");
+		}
+		System.out.println("");
+		for(int i = 0; i<matriz.length; i++) {
+			for(int j = 0; j<matriz.length; j++) {
+				ColoresTablero(matriz[i][j]);
+			}
+			System.out.println("    |"+i);
+		}
+		System.out.println("");
+		for(int i = 0; i<getColumnas();i++) {
+			System.out.print("---");
+		}
+		System.out.println();
+		for(int i = 0; i<getColumnas();i++) {
+			System.out.print(i+"   ");
+		}
+		System.out.printf("\nVidas: %d Puntos: %d",getVidas(),getPuntos());
+	}
+	public int MaximasJugadas() {
+		int max = 0;
+		for(int i = 0; i<matriz.length; i++) {
+			for(int j = 0; j<matriz[i].length;j++) {
+				try {
+					if(matriz[i][j-1] == matriz[i+1][j] && matriz[i][j] == 0 && matriz[i][j-1]!=0 ) {
+						max += 5;
+					}
+					else if(matriz[i][j-1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j-1]!=0) {
+						max += 5;
+					}
+					else if(matriz[i][j+1] == matriz[i+1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+						max += 5;
+					}
+					else if(matriz[i][j+1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+						max += 5;
+					}
+				}
+				catch(Exception e) {
+					try {
+						if(matriz[i][j-1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j-1]!=0) {
+							max += 5;
+						}
+						else if(matriz[i][j+1] == matriz[i+1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+							max += 5;
+						}
+						else if(matriz[i][j+1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+							max += 5;
+						}
+					}
+					catch(Exception x) {
+						try {
+							if(matriz[i][j+1] == matriz[i+1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+								max += 5;
+							}
+							else if(matriz[i][j+1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+								max += 5;
+							}
+						}
+						catch(Exception y) {
+							try {
+								if(matriz[i][j+1] == matriz[i-1][j] && matriz[i][j] == 0 && matriz[i][j+1]!=0) {
+									max += 5;
+								}
+							}
+							catch(Exception z) {
+								j++;
+							}
+						}
+					}
+				}
+			}
+		}
+		return max;
+	}
+	public int[][] Jugada(int jf,int jc){
+		try {
+			if(matriz[jf][jc-1] == matriz[jf+1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc-1]!=0) {
+				int puntos=getPuntos();
+				setPuntos(puntos+5);
+				matriz[jf][jc-1]=0;
+				matriz[jf+1][jc]=0;
+			}
+			else if(matriz[jf][jc-1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc-1]!=0) {
+				int puntos=getPuntos();
+				setPuntos(puntos+5);
+				matriz[jf][jc-1]=0;
+				matriz[jf-1][jc]=0;
+			}
+			else if(matriz[jf][jc+1] == matriz[jf+1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+				int puntos=getPuntos();
+				setPuntos(puntos+5);
+				matriz[jf][jc+1] = 0;
+				matriz[jf+1][jc] = 0;
+			}
+			else if(matriz[jf][jc+1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+				int puntos=getPuntos();
+				setPuntos(puntos+5);
+				matriz[jf][jc+1] = 0;
+				matriz[jf-1][jc] = 0;
+			}
+			else {
+				int vidas = getVidas();
+				setVidas(vidas-1);
+			}
+		}
+		catch(Exception e) {
+			try {
+				if(matriz[jf][jc-1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc-1]!=0) {
+					int puntos=getPuntos();
+					setPuntos(puntos+5);
+					matriz[jf][jc-1]=0;
+					matriz[jf-1][jc]=0;
+				}
+				else if(matriz[jf][jc+1] == matriz[jf+1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+					int puntos=getPuntos();
+					setPuntos(puntos+5);
+					matriz[jf][jc+1] = 0;
+					matriz[jf+1][jc] = 0;
+				}
+				else if(matriz[jf][jc+1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+					int puntos=getPuntos();
+					setPuntos(puntos+5);
+					matriz[jf][jc+1] = 0;
+					matriz[jf-1][jc] = 0;
+				}
+				else {
+					int vidas = getVidas();
+					setVidas(vidas-1);
+				}
+			}
+			catch(Exception x) {
+				try {
+					if(matriz[jf][jc+1] == matriz[jf+1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+						int puntos=getPuntos();
+						setPuntos(puntos+5);
+						matriz[jf][jc+1] = 0;
+						matriz[jf+1][jc] = 0;
+					}
+					else if(matriz[jf][jc+1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+						int puntos=getPuntos();
+						setPuntos(puntos+5);
+						matriz[jf][jc+1] = 0;
+						matriz[jf-1][jc] = 0;
+					}
+					else {
+						int vidas = getVidas();
+						setVidas(vidas-1);
+					}
+				}
+				catch(Exception y) {
+					try {
+						if(matriz[jf][jc+1] == matriz[jf-1][jc] && matriz[jf][jc] == 0 && matriz[jf][jc+1]!=0) {
+							int puntos=getPuntos();
+							setPuntos(puntos+5);
+							matriz[jf][jc+1] = 0;
+							matriz[jf-1][jc] = 0;
+						}
+						else {
+							int vidas = getVidas();
+							setVidas(vidas-1);
+						}
+					}
+					catch(Exception z) {
+						int vidas = getVidas();
+						setVidas(vidas-1);
+					}
+				}
+			}
+		}
+		return matriz;
+	}
+	
+}
