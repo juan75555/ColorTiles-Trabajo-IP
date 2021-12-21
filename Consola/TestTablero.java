c6a80c11247aeead368360b62a017044df324d04
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 17:27:36 +0100

    Update TestTablero.java

--- Consola/TestTablero.java
+++ Consola/TestTablero.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class TestTablero {
 	public static void Juego(Tablero t,int[][] tablero) {
 		Scanner teclado = new Scanner(System.in);
-		while(t.getVidas()>0 && t.getPuntos()<6) {
+		System.out.print("\nMaximos Puntos Posibles: "+t.MaximasJugadas()+"\n");
+		while(t.getVidas()>0 && t.getPuntos()<t.MaximasJugadas()) {
 			System.out.print("\nIntroduce tu jugada (fila, columna): ");
 			int jf = teclado.nextInt();
 			int jc = teclado.nextInt();
 			t.Jugada(jf, jc);
 			t.ImprimeTablero(tablero);
+			System.out.print("\nMaximos Puntos Posibles: "+t.MaximasJugadas()+"\n");
 		}
 		if (t.getVidas() == 0) {
 			System.out.print("\n\033[31m YOU LOSE!!! :(");
 		}
-		if (t.getPuntos() >5) {
+		if (t.getPuntos() >=t.MaximasJugadas()) {
 			System.out.print("\n\033[32m YOU WIN!!! :)");
 		}
 	}
