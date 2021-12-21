dc76a6307d64d9aa68c87ae215bfcc3c28de597b
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 15:17:30 +0100

    Create TestTablero.java

---
+++ Consola/TestTablero.java
@@ -0,0 +1,10 @@
+
+public class TestTablero {
+
+	public static void main(String[] args) {
+		Tablero t = new Tablero();
+		int[][] tablero = t.GeneraTablero();
+		t.ImprimeTablero(tablero);
+	}
+
+}
