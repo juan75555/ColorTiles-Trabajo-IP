bc89e8072e5fb68e0afc8dfc1d71d2eee91210c0
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 22:45:43 +0100

    Update TestTablero.java

--- Consola/TestTablero.java
+++ Consola/TestTablero.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TestTablero {
+public class Main {
 	public static void Juego(Tablero t,int[][] tablero) {
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("\nMaximos Puntos Posibles: "+t.MaximasJugadas()+"\n");
@@ -12,19 +12,32 @@ public class TestTablero {
 			System.out.print("\nMaximos Puntos Posibles: "+t.MaximasJugadas()+"\n");
 		}
 		if (t.getVidas() == 0) {
-			System.out.print("\n\033[31m YOU LOSE!!! :(");
+			System.out.printf("\n\033[31m %s LOSE!!! :(",t.getName());
 			teclado.close();
 		}
 		if (t.getPuntos() >=t.MaximasJugadas()) {
-			System.out.print("\n\033[32m YOU WIN!!! :)");
+			System.out.printf("\n\033[32m %s WIN!!! :)",t.getName());
 			teclado.close();
 		}
 	}
 	public static void main(String[] args) {
 		Tablero t = new Tablero();
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Deseas jugar como invitado? (S/N): ");
+		String op = teclado.next();
+		while(!(op.equals("S") || op.equals("N"))) {
+			System.out.print("Deseas jugar como invitado? (S/N): ");
+			op = teclado.next();
+		}
+		if(op.equals("N")) {
+			System.out.print("Introduce tu nombre: ");
+			String name = teclado.next();
+			t.setName(name);
+		}
 		int[][] tablero = t.GeneraTablero();
 		t.ImprimeTablero(tablero);
 		Juego(t,tablero);
+		teclado.close();
 	}
 
 }
