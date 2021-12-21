36dab3282d20f6b44f074356614104d39de3e0d1
juan75555 <72112435+juan75555@users.noreply.github.com>
2021-12-21 23:40:00 +0100

    Update TestTablero.java

--- Consola/TestTablero.java
+++ Consola/TestTablero.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void Juego(Tablero t,int[][] tablero) {
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("\nMaximos Puntos Posibles: "+t.MaximasJugadas()+"\n");
-		while(t.getVidas()>0 && t.MaximasJugadas()>0){
+		while(t.getVidas()>0 && t.MaximasJugadas()>0) {
 			System.out.print("\nIntroduce tu jugada (fila, columna): ");
 			int jf = teclado.nextInt();
 			int jc = teclado.nextInt();
@@ -33,6 +33,29 @@ public class Main {
 			System.out.print("Introduce tu nombre: ");
 			String name = teclado.next();
 			t.setName(name);
+		}
+		System.out.printf("El tamaño del tablero actualmente es %d x %d.\n Deseas cambiarlo?(S/N): ", t.getFilas(),t.getColumnas());
+		op = teclado.next();
+		while(!(op.equals("S") || op.equals("N"))) {
+			System.out.print("Deseas jugar como invitado? (S/N): ");
+			op = teclado.next();
+		}
+		if(op.equals("S")) {
+			System.out.print("Introduce el nuevo numero de filas: ");
+			int filas = teclado.nextInt();
+			System.out.print("Introduce el nuevo numero de columnas: ");
+			int columnas = teclado.nextInt();
+			while(filas<1) {
+				System.out.printf("\nFilas introducidas %d debe ser mayor que 0.",filas);
+				System.out.print("\nIntroduce el nuevo numero de filas: ");
+				filas = teclado.nextInt();
+			}
+			while(columnas<1) {
+				System.out.printf("\nColumnas introducidas %d debe ser mayor que 0.",columnas);
+				System.out.print("\nIntroduce el nuevo numero de columnas: ");
+				columnas = teclado.nextInt();
+			}
+			t = new Tablero(filas,columnas);
 		}
 		int[][] tablero = t.GeneraTablero();
 		t.ImprimeTablero(tablero);
