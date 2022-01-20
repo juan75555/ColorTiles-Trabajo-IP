2e9f085a7025c967aa5fc33d0718faf9b48d9274
juan75555 <72112435+juan75555@users.noreply.github.com>
2022-01-20 15:35:04 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,44 @@ package juego;
 
 import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
-	/**
+	/**º
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Juego frame = new Juego();
+					String name=
+							JOptionPane.showInputDialog("Introduce tu nombre: ");
+					String ndificultad=
+							JOptionPane.showInputDialog("Introduce la dificultad (0.1-0.9):  ");
+					double dificultad = Double.parseDouble(ndificultad);
+					while(dificultad<=0 || dificultad>=1) {
+						ndificultad=
+								JOptionPane.showInputDialog("Introduce la dificultad (0.1-0.9):  ");
+						dificultad = Double.parseDouble(ndificultad);
+					}
+					String nfilas=
+							JOptionPane.showInputDialog("Introduce el nuevo numero de filas:  ");
+					int filas = Integer.parseInt(nfilas);
+					String ncol=
+							JOptionPane.showInputDialog("Introduce el nuevo numero de columnas:  ");
+					int columnas = Integer.parseInt(ncol);
+					while(filas<1) {
+						nfilas=
+								JOptionPane.showInputDialog("Introduce el nuevo numero de filas:  ");
+						filas = Integer.parseInt(nfilas);
+					}
+					while(columnas<1) {
+						ncol=
+								JOptionPane.showInputDialog("Introduce el nuevo numero de columnas:  ");
+						columnas = Integer.parseInt(ncol);
+					}
+					Juego frame = new Juego(name,dificultad,filas,columnas);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
