d483e621c7d3180c46304a38a417b8b8aed3788b
juan75555 <72112435+juan75555@users.noreply.github.com>
2022-01-20 13:27:05 +0100

    Update and rename TestTablero.java to Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+package juego;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Juego frame = new Juego();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
